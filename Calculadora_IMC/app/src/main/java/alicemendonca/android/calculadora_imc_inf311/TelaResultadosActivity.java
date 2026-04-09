422de70c47c9a18b7cca6be57cec3a02aa31c0b9
lilicesm <alicesm3012@gmail.com>
2026-04-09 09:25:10 -0300

    protótipo do layout das telas

--- Calculadora_IMC/app/src/main/java/alicemendonca/android/calculadora_imc_inf311/TelaResultadosActivity.java
+++ Calculadora_IMC/app/src/main/java/alicemendonca/android/calculadora_imc_inf311/TelaResultadosActivity.java
@@ -1,7 +1,6 @@
 package alicemendonca.android.calculadora_imc_inf311;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,24 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class TelaResultadosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.tela_resultados);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-//    Button navegacao = findViewById(R.id.btnRelatorio);
-//
-//    navegacao.setOnClickListener(new View.OnClickListener() {
-//        @Override
-//            public void
-//    })
 }
