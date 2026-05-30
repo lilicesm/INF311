27e3c98d9aac1df5e967c8322709eaa90bff2cb5
lilicesm <alicesm3012@gmail.com>
2026-05-30 01:14:29 -0300

    feat: botao do appB devolve valores pro appA

--- Pratica04_B/app/src/main/java/alicemendonca/android/pratica04_b/MainActivity.java
+++ Pratica04_B/app/src/main/java/alicemendonca/android/pratica04_b/MainActivity.java
@@ -1,6 +1,9 @@
 package alicemendonca.android.pratica04_b;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,47 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnDevolver;
+    private float lumRecebida = -1;
+    private float distRecebida = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnDevolver = (Button) findViewById(R.id.btnDevolver);
+
+        Intent itRecebida = getIntent();
+        if (itRecebida != null) {
+            lumRecebida = itRecebida.getFloatExtra("luminosidade", -1);
+            distRecebida = itRecebida.getFloatExtra("distancia", -1);
+        }
+
+        btnDevolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String resultadoLum;
+                String resultadoDist;
+
+                if (lumRecebida < 20.0f) {
+                    resultadoLum = "baixa";
+                } else {
+                    resultadoLum = "alta";
+                }
+
+                if (distRecebida > 3.0f) {
+                    resultadoDist = "distante";
+                } else {
+                    resultadoDist = "perto";
+                }
+
+                Intent itResposta = new Intent();
+                itResposta.putExtra("resultado_lum", resultadoLum);
+                itResposta.putExtra("resultado_dist", resultadoDist);
+                setResult(RESULT_OK, itResposta);
+                finish();
+            }
         });
     }
 }
